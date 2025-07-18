eb417b3ac17398d8a39fb0aeaeb2c29cd2dab2a5
BISNISCREATIVE <bisniscreative4.0@gmail.com>
2025-07-19 04:00:10 +0700

    Delete client directory

--- client/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// Lib utilities index for cleaner imports
-
-export { default as apiClient } from "./api";
-export { queryClient } from "./query-client";
-export { validateFile, createFilePreview } from "./upload";
-export { cn } from "./utils";
