a6e7f109468868ad23e35e28547ae215b3096ee6
BISNISCREATIVE <bisniscreative4.0@gmail.com>
2025-07-18 18:10:03 +0700

    Delete client directory

--- client/components/index.ts
+++
@@ -1,8 +0,0 @@
-// Main components export index
-// Import everything from essentials for cleaner imports throughout the app
-
-export * from "./essentials";
-
-// Re-export commonly used hooks
-export { useAuth } from "../hooks/use-auth";
-export { useIsMobile as useMobile } from "../hooks/use-mobile";
